ae9d37afd96fe3dd9c497d156b1e0baf0c34ebaf
Владислав Суворов <gordik_08@mail.ru>
2023-06-26 20:22:10 +0300

    8

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,4 +1,4 @@
-# flake8: noqa
+# flake8: noqad
 import os
 from pathlib import Path
 
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'gordik89.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
