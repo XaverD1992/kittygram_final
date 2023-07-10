454024eeb877ec32a0dda773a7549adfa7a7007d
Владислав Суворов <gordik_08@mail.ru>
2023-07-10 14:10:35 +0300

    22

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='ddd')
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', default='True')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1').split(',')
 
