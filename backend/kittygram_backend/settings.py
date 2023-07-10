18cec9caea8658e2087db3a60ac3f339d47f4862
Владислав Суворов <gordik_08@mail.ru>
2023-07-10 13:11:14 +0300

    17

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='ddd')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'gordik89.ddns.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
